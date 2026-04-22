7451c74a723af4c15450d39bf1f4cd8141607bca
qq928672 <70316969+qq928672@users.noreply.github.com>
2026-04-22 20:01:06 +0800

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/schedule-app/', // 改成你的 repo 名稱
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
